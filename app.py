623d62e44ea30623920fc13234dd80f64884ddc7
bughouse-wizzard <ddima.makarenko@gmail.com>
2026-01-14 18:34:39 +0300

    Initial commit for Service Beta

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/inventory/<item_id>', methods=['GET'])
+def get_inventory(item_id):
+    # Simulating a database lookup
+    # In a real app, this would query a DB
+    return jsonify({
+        "item_id": item_id,
+        "stock": 5  # Fixed stock for simplicity
+    })
+
+if __name__ == '__main__':
+    app.run(port=5002)
